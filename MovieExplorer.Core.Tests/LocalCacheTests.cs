d0c6490c4d9c62a77134f2d1137965001dbdcb25
Dean Faizal <deanfaizal@gmail.com>
2016-08-12 21:18:23 -0400

    Implemented Akavache, local cache, and tests

---
+++ MovieExplorer.Core.Tests/LocalCacheTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieExplorer.Core.DataAccessLayer;
+using MovieExplorer.Core.ServiceLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieExplorer.Core.Tests
+{
+    [TestClass]
+    public class LocalCacheTests
+    {
+        [TestMethod]
+        public async Task SaveAndLoadTest()
+        {
+            var id = 12345;
+            var title = "Test Movie I";
+            var testMovie = new Movie { Id = id, Title = title };
+
+            //Save
+            await LocalCache.Instance.Save(testMovie.Id.ToString(), testMovie);
+            testMovie = null;
+            Assert.IsNull(testMovie);
+
+            //Load
+            testMovie = await LocalCache.Instance.Load<Movie>(id.ToString());
+
+            Assert.IsNotNull(testMovie);
+            Assert.AreEqual(id, testMovie.Id);
+            Assert.AreEqual(title, testMovie.Title);
+        }
+    }
+}
---
+++ MovieExplorer.Core/DataAccessLayer/LocalCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using Akavache;
+
+namespace MovieExplorer.Core.DataAccessLayer
+{
+   public class LocalCache
+    {
+        private static bool _isInitialized = false;
+        private static Lazy<LocalCache> _instance = new Lazy<LocalCache>();
+
+        public static LocalCache Instance
+        {
+            get {
+                if (!_isInitialized)
+                {
+                    BlobCache.ApplicationName = "MovieExplorer";
+                    _isInitialized = true;
+                }
+                return _instance.Value; }
+        }
+
+        public async Task Save<T>(string id, T item)
+        {
+            await BlobCache.UserAccount.InsertObject(id, item);
+        }
+
+        public async Task<T> Load<T>(string id)
+        {
+            return await BlobCache.UserAccount.GetObject<T>(id);
+        }
+    }
+}
