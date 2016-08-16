2ddb9406ceb389258c51c4fff5f8654f751ab8bf
dcdroid-mac <fakhruddin087@gmail.com>
2016-08-16 00:36:50 -0400

    * MovieExplorer.iOS.UITests.csproj: Updated ui test project
    * Tests.cs: Added automated UI tests for launching the app, opening
      app details, opening similar movie details, and searching for a
      movie
    
    * AppDelegate.cs: Added calabash
    
    * packages.config:
    * MovieExplorer.iOS.csproj: Added ui test packages

--- MovieExplorer.iOS.UITests/Tests.cs
+++ MovieExplorer.iOS.UITests/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using MovieExplorer.Core.ServiceLayer.Model;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.iOS;
@@ -8,30 +9,54 @@ using Xamarin.UITest.Queries;
 
 namespace iOSUITest
 {
-    [TestFixture]
-    public class Tests
-    {
-        iOSApp app;
+	[TestFixture]
+	public class Tests
+	{
+		iOSApp app;
 
-        [SetUp]
-        public void BeforeEachTest()
-        {
-            // TODO: If the iOS app being tested is included in the solution then open
-            // the Unit Tests window, right click Test Apps, select Add App Project
-            // and select the app projects that should be tested.
-            app = ConfigureApp
-                .iOS
-                // TODO: Update this path to point to your iOS app and uncomment the
-                // code if the app is not included in the solution.
-                //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/iOSUITest.iOS.app")
-                .StartApp();
-        }
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			// TODO: If the iOS app being tested is included in the solution then open
+			// the Unit Tests window, right click Test Apps, select Add App Project
+			// and select the app projects that should be tested.
+			app = ConfigureApp
+				.iOS
+				.EnableLocalScreenshots ()
+				// TODO: Update this path to point to your iOS app and uncomment the
+				// code if the app is not included in the solution.
+				.AppBundle ("../../../MovieExplorer.iOS/bin/iPhoneSimulator/Debug/MovieExploreriOS.app")
+				.StartApp ();
+		}
 
-        [Test]
-        public void AppLaunches()
-        {
-            app.Screenshot("First screen.");
-        }
-    }
+		[Test]
+		public void AppLaunches ()
+		{
+			app.Screenshot ("Main screen");
+		}
+
+		[Test]
+		public void OpenDetails ()
+		{
+			app.Tap (a => a.Class ("MovieExplorer_iOS_UILayer_Controls_MovieCell"));
+			app.Screenshot ("Details screen");
+		}
+
+		[Test]
+		public void OpenSimilarMovieDetails ()
+		{
+			app.Tap (a => a.Class ("MovieExplorer_iOS_UILayer_Controls_MovieCell"));
+			app.Tap (a => a.Class ("MovieExplorer_iOS_UILayer_Controls_MovieCell"));
+			app.Screenshot ("Similar movies details screen");
+		}
+
+		[Test]
+		public void SearchForMovie ()
+		{
+			app.Tap (a => a.Marked ("Search"));
+			app.EnterText (a => a.Class ("UISearchBar"), "Interstellar");
+			app.Screenshot ("Searched for Interstellar");
+		}
+	}
 }
 
